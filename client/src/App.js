851b6af0709652af4ca0c98a0b75192261084779
ArpitaDeb <arpitadeb1990@gmail.com>
2020-07-22 19:32:39 -0400

    frontend proxy added

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,15 @@ import useApplicationData from './hooks/useApplicationData';
 import './App.css';
 
 function App() {
-
   const { state, setState } = useApplicationData();
-
-
   const userList = state.users.map(user => <li key={user.id}> {user.name} {user.email}</li>)
 
   return (
     <>
       <h1>Users</h1>
-
       <ul>
         {userList}
       </ul>
-
     </>
   );
 }
