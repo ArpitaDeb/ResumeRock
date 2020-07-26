da4f10f0ec1116552260589a46e2f84a833e44b1
ArpitaDeb <arpitadeb1990@gmail.com>
2020-07-26 13:40:34 -0400

    removed tabs folder

--- client/src/components/LeftSidebar/LeftSidebar.js
+++ client/src/components/LeftSidebar/LeftSidebar.js
@@ -2,18 +2,18 @@ import React from "react";
 import { Nav } from "react-bootstrap";
 export default function LeftSidebar(props) {
   return (
-  
-      <Nav className="col-md-12 d-none d-md-block bg-light sidebar"
-        activeKey="/home"
-        onSelect={selectedKey => alert(`selected ${ selectedKey } `)}
-      >
-        <div className="sidebar-sticky"></div>
-        {props.items.map((item) => (
-          <Nav.Item>
-            <Nav.Link eventKey="link">{item.title}</Nav.Link>
-          </Nav.Item>
-        ))}
-      </Nav>
-    
+
+    <Nav className="col-md-12 d-none d-md-block bg-light sidebar"
+      activeKey="/home"
+      onSelect={selectedKey => alert(`selected ${selectedKey} `)}
+    >
+      <div className="sidebar-sticky"></div>
+      {props.items.map((item) => (
+        <Nav.Item>
+          <Nav.Link eventKey="link"><i className="fa fa-envelope fa-fw"></i>{item.title}</Nav.Link>
+        </Nav.Item>
+      ))}
+    </Nav>
+
   );
 };
