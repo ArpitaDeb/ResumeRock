32e2cc0dc778ded391cb28b2253c0f1842be3d43
ArpitaDeb <arpitadeb1990@gmail.com>
2020-07-27 00:04:33 -0400

    Personal Information implemented

--- client/src/stories/FormContainer.stories.js
+++ client/src/stories/FormContainer.stories.js
@@ -3,6 +3,7 @@ import React from 'react';
 import FormContainer from "../components/FormContainer";
 import { action } from '@storybook/addon-actions';
 import SummaryForm from "../components/forms/SummaryForm";
+import PersonalInfoForm from "../components/forms/PersonalInfoForm";
 
 export default {
   title: 'FormContainer',
@@ -13,3 +14,19 @@ export const SummaryInContainer = () =>
   <FormContainer title="Summary" >
     <SummaryForm data={{ heading: "Summary", body: "Experienced customer relationship professional "}} onUpdate={action('Update Received')} />
   </FormContainer>
+export const PersonalInfoInContainer = () =>
+<FormContainer title="Personal Information" >
+  <PersonalInfoForm data={{
+    firstName: "",
+    lastName: "",
+    profTitle: "",
+    email: "",
+    linkedIn: "",
+    phoneNumber: "",
+    addressLine1: "",
+    addressLine2: "",
+    formGridcity: "",
+    formGridProvince: "",
+    formGridZip: ""
+  }} onUpdate={action('Update Received')} />
+</FormContainer>
