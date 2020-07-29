38dc91e422c390930ec34e6b227813f95316d174
ArpitaDeb <arpitadeb1990@gmail.com>
2020-07-29 00:16:53 -0400

    left side bar modified

--- client/src/components/LeftSidebar/LeftSidebar.js
+++ client/src/components/LeftSidebar/LeftSidebar.js
@@ -6,15 +6,14 @@ export default function LeftSidebar(props) {
   const selectedKey = props.onUpdate;
   return (
     <Nav className="col-md-12 d-none d-md-block bg-light sidebar"
-      activeKey="/home"
     >
-      <div className="sidebar-sticky"></div>
-      {props.items.map((item) => (
-        <Nav.Item onSelect={selectedKey}>
-          <Nav.Link eventKey="link">{item.title}</Nav.Link>
-        </Nav.Item>
-      ))}
+      <div className="sidebar-sticky">
+        {props.items.map((item) => (
+          <Nav.Item onSelect={selectedKey}>
+            <Nav.Link eventKey="link">{item.title}</Nav.Link>
+          </Nav.Item>
+        ))}
+      </div>
     </Nav>
-
   );
 };
