3a1c0defbdcaf3fad9605e886c964c7bee99351e
Ghazal <50920748+Ghazall1993@users.noreply.github.com>
2020-08-09 22:00:03 -0400

    modify register form (#36)

--- client/src/components/userLR/Login.js
+++ client/src/components/userLR/Login.js
@@ -46,46 +46,46 @@ export default function Login(props) {
 
   return (
     <>
-        <div className="background" />
-        <div className="login-container">
-          <Card className="shadow border-0 d-inline-block p-5">
-            <CardBody>
-              <div >
+      <div className="background" />
+      <div className="login-container">
+        <Card className="shadow border-0 d-inline-block p-5">
+          <CardBody>
+            <div >
               <div className="avatar">
-                    <img src={avatar} alt="Avatar"/>
-				        </div>
-                <h3 className="text-center">Member Login
+                <img src={avatar} alt="Avatar" />
+              </div>
+              <h3 className="text-center">Member Login
                 </h3>
-                  <form onSubmit={handleSubmit}>
-                    <FormGroup controlId="email" bssize="large" className="form_inputs">
-                      <FormLabel />
-                      <FormControl
-                        autoFocus
-                        type="email"
-                        value={email}
-                        placeholder="Email"
-                        onChange={(event) => setEmail(event.target.value)}
-                      />
-                    </FormGroup>
-                    <FormGroup controlId="password" bssize="large" className="form_inputs">
-                      <FormLabel />
-                      <FormControl
-                        placeholder="Password"
-                        value={password}
-                        onChange={(event) => setPassword(event.target.value)}
-                        type="password"
-                      />
-                    </FormGroup>
-                    <Button block bssize="large" type="submit" className="mt-4" >
-                      Login
+              <form onSubmit={handleSubmit}>
+                <FormGroup controlId="email" bssize="large" className="form_inputs">
+                  <FormLabel />
+                  <FormControl
+                    autoFocus
+                    type="email"
+                    value={email}
+                    placeholder="Email"
+                    onChange={(event) => setEmail(event.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup controlId="password" bssize="large" className="form_inputs">
+                  <FormLabel />
+                  <FormControl
+                    placeholder="Password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                    type="password"
+                  />
+                </FormGroup>
+                <Button block bssize="large" type="submit" className="mt-4" >
+                  Login
                   </Button>
 
-                    <div className='error'>{error}</div>
-                  </form>
-              </div>
-            </CardBody>
-          </Card>
-        </div>
+                <div className='error'>{error}</div>
+              </form>
+            </div>
+          </CardBody>
+        </Card>
+      </div>
 
 
 
--- client/src/components/userLR/Registration.js
+++ client/src/components/userLR/Registration.js
@@ -3,12 +3,19 @@ import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import './Login.css';
+import {
+  Card,
+  CardBody,
+} from "reactstrap";
+import avatar from "../../img/avatar.png";
+
 
 export default function Registration({ isLoggedIn, setIsLoggedIn }) {
   const [state, setState] = useState({
     email: "",
     password: "",
-    userName: ""
+    userName: "",
+    confirm_password: ""
   })
   const [error, setError] = useState("");
   const handleChange = (e) => {
@@ -19,14 +26,14 @@ export default function Registration({ isLoggedIn, setIsLoggedIn }) {
     }))
   }
   const validateForm = () => {
-    return state.email.length > 0 && state.password.length > 0 && state.userName.length > 0;
+    return state.email.length > 0 && state.password.length > 0 && state.password === state.confirm_password;
   }
   const history = useHistory();
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(state);
     axios.post(
-      '/users/register', { email: state.email, password: state.password, userName: state.userName }
+      '/users/register', { email: state.email, password: state.password, userName: state.email }
     ).then(() => {
       history.push('/');
       setIsLoggedIn(true);
@@ -35,38 +42,56 @@ export default function Registration({ isLoggedIn, setIsLoggedIn }) {
 
   return (
     <>
-      <div className="login">
-        <form onSubmit={handleSubmit}>
-          <FormGroup controlId="email" bssize="large">
-            <FormLabel>Email</FormLabel>
-            <FormControl
-              autoFocus
-              type="email"
-              value={state.email}
-              onChange={handleChange}
-            />
-          </FormGroup>
-          <FormGroup controlId="password" bssize="large">
-            <FormLabel>Password</FormLabel>
-            <FormControl
-              value={state.password}
-              onChange={handleChange}
-              type="password"
-            />
-          </FormGroup>
-          <FormGroup controlId="userName" bssize="large">
-            <FormLabel>User Name</FormLabel>
-            <FormControl
-              value={state.userName}
-              onChange={handleChange}
-              type="text"
-            />
-          </FormGroup>
-          <Button block bssize="large" disabled={!validateForm()} type="submit">
-            Register
+      <div className="background" />
+      <div className="login-container">
+        <Card className="shadow border-0 d-inline-block p-5">
+          <CardBody>
+            <div >
+              <div className="avatar">
+                <img src={avatar} alt="Avatar" />
+              </div>
+              <h3 className="text-center">Register Now!
+                </h3>
+
+              <form onSubmit={handleSubmit}>
+                <FormGroup controlId="email" bssize="large" className="form_inputs">
+                  <FormLabel />
+                  <FormControl
+                    placeholder="Email"
+                    autoFocus
+                    type="email"
+                    value={state.email}
+                    onChange={handleChange}
+                  />
+                </FormGroup>
+                <FormGroup controlId="password" bssize="large" className="form_inputs">
+                  <FormLabel />
+                  <FormControl
+                    placeholder="Password"
+                    value={state.password}
+                    onChange={handleChange}
+                    type="password"
+                  />
+                </FormGroup>
+
+                <FormGroup controlId="confirm_password" bssize="large" className="form_inputs">
+                  <FormLabel />
+                  <FormControl
+                    placeholder="Confirm your password"
+                    value={state.confirm_password}
+                    onChange={handleChange}
+                    type="password"
+                  />
+                </FormGroup>
+
+                <Button block bssize="large" disabled={!validateForm()} type="submit" className="mt-4" >
+                  Register
         </Button>
-          <div className='error'>{error}</div>
-        </form>
+                <div className='error'>{error}</div>
+              </form>
+            </div>
+          </CardBody>
+        </Card>
       </div>
     </>
   );
