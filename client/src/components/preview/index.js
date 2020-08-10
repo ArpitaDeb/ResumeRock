1a4269444adef53c878c8d78610e2e325fc048e9
Ghazal <50920748+Ghazall1993@users.noreply.github.com>
2020-08-10 18:24:14 -0400

    add fullscreen feature (#40)

--- client/src/components/preview/index.js
+++ client/src/components/preview/index.js
@@ -6,12 +6,13 @@ import fileDownload from 'js-file-download';
 import { Upload } from "./Upload";
 
 export default function Preview(props) {
-//   const [showImportBtn, setShowImportBtn] = useState(true);
+
   const [showUpload, setShowUpload] = useState(false);
   const [showMenu, setShowMenu] = useState(true);
   const [fullscreen, setFullscreen] = useState(false);
 
   const showImportBtn = () => Object.keys(props.data).length === 0
+
   const exportJSON = useCallback(() => {
     fileDownload(JSON.stringify(props.data), 'resume.json');
   }, [props.data])
@@ -19,6 +20,11 @@ export default function Preview(props) {
     setShowMenu(true);
     setShowUpload(false)
     props.onUpdate(JSON.parse(data))
+  }
+
+  const handleClose = (e) => {
+    e.target.style.display = 'none';
+    setFullscreen(false);
   }
   return (
     <div>
@@ -33,7 +39,7 @@ export default function Preview(props) {
               <Button
                 disabled={showImportBtn()}
                 variant="light"
-                onClick={() => setFullscreen(true)}
+                onClick={() => { setFullscreen(true); console.log("HELLO") }}
               >
                 Fullscreen
             </Button>
@@ -46,7 +52,7 @@ export default function Preview(props) {
           </Button>
               <Button
                 variant="light"
-                onClick={() => {setShowMenu(false); setShowUpload(true) }}
+                onClick={() => { setShowMenu(false); setShowUpload(true) }}
               >
                 Import JSON
             </Button>
@@ -56,6 +62,15 @@ export default function Preview(props) {
             <CustomerServiceTemplate data={props.data} />
           </div>
         </>
+      }
+      {
+        fullscreen &&
+        <div className="overlay">
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+           <CustomerServiceTemplate data={props.data} />
+        </div>
       }
       <div >
         {showUpload ? <Upload onFileImported={onFileImported} /> : <CustomerServiceTemplate data={props.data} />}
