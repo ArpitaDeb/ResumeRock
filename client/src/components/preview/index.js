a440016e0abc506fc6c09367ad769d0ed110acf8
Ghazall1993 <ghazal.fouladfard@gmail.com>
2020-08-10 02:27:49 -0400

    handle the routes in preview section

--- client/src/components/preview/index.js
+++ client/src/components/preview/index.js
@@ -6,38 +6,56 @@ import fileDownload from 'js-file-download';
 import { Upload } from "./Upload";
 
 export default function Preview(props) {
+  const [showImportBtn, setShowImportBtn] = useState(true);
   const [showUpload, setShowUpload] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
   const exportJSON = useCallback(() => {
     fileDownload(JSON.stringify(props.data), 'resume.json');
   }, [props.data])
   const onFileImported = (data) => {
+    debugger
+    setShowImportBtn(false)
+    setShowMenu(true);
     setShowUpload(false)
     props.onUpdate(JSON.parse(data))
   }
 
   return (
     <div>
-      <div className="bar-container">
-        <ButtonGroup className="previw-bar">
-          <Button variant="light">Export to PDF</Button>
-          <Button variant="light">Fullscreen</Button>
-          <Button
-            variant="light"
-            onClick={exportJSON}
-          >
-            Export JSON
+      {showMenu ?
+        <div className="bar-container">
+          <ButtonGroup className="previw-bar">
+            <Button variant="light">Export to PDF</Button>
+            <Button variant="light">Fullscreen</Button>
+            <Button
+              variant="light"
+              onClick={exportJSON}
+            >
+              Export JSON
           </Button>
-          <Button
-            variant="light"
-            onClick={() => { setShowUpload(true) }}
-          >
-            Import JSON
+            <Button
+              variant="light"
+              onClick={() => {setShowImportBtn(false) ; setShowUpload(true)} }
+            >
+              Import JSON
             </Button>
-        </ButtonGroup >
-      </div>
+          </ButtonGroup >
+        </div>
+        : showImportBtn ?
+          <div className="bar-container">
+            <ButtonGroup className="previw-bar">
+              <Button
+                variant="light"
+                onClick={() => { setShowImportBtn(false) ; setShowUpload(true)}}
+              >
+                Import JSON
+            </Button>
+            </ButtonGroup>
+          </div> : ""
+}
       <div >
-        {showUpload ? <Upload onFileImported={onFileImported} /> : <CustomerServiceTemplate data={props.data} />}
+        {showUpload ? <Upload onFileImported={onFileImported} /> : showMenu ? <CustomerServiceTemplate data={props.data} /> : ""}
       </div>
     </div >
   )
