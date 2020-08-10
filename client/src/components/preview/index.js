2931f29c96c30ac0313dce4f922a20c36a14defb
Ghazall1993 <ghazal.fouladfard@gmail.com>
2020-08-10 03:26:48 -0400

    fix issues

--- client/src/components/preview/index.js
+++ client/src/components/preview/index.js
@@ -6,58 +6,60 @@ import fileDownload from 'js-file-download';
 import { Upload } from "./Upload";
 
 export default function Preview(props) {
-  const [showImportBtn, setShowImportBtn] = useState(true);
+//   const [showImportBtn, setShowImportBtn] = useState(true);
   const [showUpload, setShowUpload] = useState(false);
-  const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(true);
+  const [fullscreen, setFullscreen] = useState(false);
 
+  const showImportBtn = () => Object.keys(props.data).length === 0
   const exportJSON = useCallback(() => {
     fileDownload(JSON.stringify(props.data), 'resume.json');
   }, [props.data])
   const onFileImported = (data) => {
-    debugger
-    setShowImportBtn(false)
     setShowMenu(true);
     setShowUpload(false)
     props.onUpdate(JSON.parse(data))
   }
-
   return (
     <div>
-      {showMenu ?
-        <div className="bar-container">
-          <ButtonGroup className="previw-bar mt-2">
-            <Button variant="light">Export to PDF</Button>
-            <Button variant="light">Fullscreen</Button>
-            <Button
-              variant="light"
-              onClick={exportJSON}
-            >
-              Export JSON
-          </Button>
-            <Button
-              variant="light"
-              onClick={() => {setShowImportBtn(false) ; setShowUpload(true)} }
-            >
-              Import JSON
-            </Button>
-          </ButtonGroup >
-        </div>
-        : showImportBtn ?
+      {showMenu &&
+        <>
           <div className="bar-container">
-            <ButtonGroup className="previw-bar mt-3">
+            <ButtonGroup className="previw-bar mt-2">
               <Button
                 variant="light"
-                onClick={() => { setShowImportBtn(false) ; setShowUpload(true)}}
+                disabled={showImportBtn()}
+              >Export to PDF</Button>
+              <Button
+                disabled={showImportBtn()}
+                variant="light"
+                onClick={() => setFullscreen(true)}
+              >
+                Fullscreen
+            </Button>
+              <Button
+                disabled={showImportBtn()}
+                variant="light"
+                onClick={exportJSON}
+              >
+                Export JSON
+          </Button>
+              <Button
+                variant="light"
+                onClick={() => {setShowMenu(false); setShowUpload(true) }}
               >
                 Import JSON
             </Button>
-            </ButtonGroup>
-          </div> : ""
-}
+            </ButtonGroup >
+          </div>
+          <div>
+            <CustomerServiceTemplate data={props.data} />
+          </div>
+        </>
+      }
       <div >
-        {showUpload ? <Upload onFileImported={onFileImported} /> : showMenu ? <CustomerServiceTemplate data={props.data} /> : ""}
+        {showUpload ? <Upload onFileImported={onFileImported} /> : <CustomerServiceTemplate data={props.data} />}
       </div>
     </div >
   )
-
 }
